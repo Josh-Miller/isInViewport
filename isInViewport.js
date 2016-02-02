69fd3917b843d4f24c31ee0c4131a4358e8a032f
Steven Vachon <contact@svachon.com>
2016-02-02 10:11:47 -0500

    Improvements
    * added horizontal scroll support
    * both functions now support single elements from `document.querySelector()`
    * `inView` calls the callback for *each* element in the list, instead of only the first
    * `isInView` only returns `true` when *all* elements in the list are in view

--- isInViewport.js
+++ isInViewport.js
@@ -5,22 +5,33 @@ function isInViewport() {
 
   function __inView(el) {
     var bounds = el.getBoundingClientRect();
-    return bounds.top < window.innerHeight && bounds.bottom > 0;
+    return bounds.top < window.innerHeight && bounds.bottom > 0 && bounds.left < window.innerWidth && bounds.right > 0;
   }
 
   var self = {
     inView: function(el, cb) {
-      for (var i=0; i<el.length; i++) {
-        if (__inView(el[i])) {
-          return cb(el);
+      if (el instanceof NodeList) {
+        for (var i=0; i<el.length; i++) {
+          if (__inView(el[i])) {
+            cb(el);
+          }
         }
+      } else if (__inView(el)) {
+        cb(el);
       }
+      return el;
     },
     isInView: function(el) {
-      for (var i=0; i<el.length; i++) {
-        return __inView(el[i]);
-      };
-
+      if (el instanceof NodeList) {
+        for (var i=0; i<el.length; i++) {
+          if (!__inView(el[i])) {
+            return false;
+          }
+        }
+        return true;
+      } else {
+        return __inView(el);
+      }
     },
   };
   return self;
